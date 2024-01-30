74d01d8041265063c35629239edd47b7c5078ad0
adsouza <arkievdsouza@sydney.edu.au>
2024-01-30 16:09:09 +1100

    script that iteratively processes dir-1 and dir-2

--- ConnectomeBids_Pydrafied_v1.py
+++ ConnectomeBids_Pydrafied_v1.py
@@ -8,18 +8,18 @@ from fileformats.medimage import NiftiGzXBvec, NiftiGz
 from fileformats.medimage_mrtrix3 import ImageFormat
 
 # Define the path and output_path variables
-output_path = '/Users/arkievdsouza/git/ConnectomeBids_Pydrafied/outputs2/'
+output_path = '/Users/arkievdsouza/git/ConnectomeBids_Pydrafied/outputs/'
 
 # Define the input_spec for the workflow
-input_spec = {"dwi": NiftiGz, "bvec": File, "bval": File, "json": File}
+input_spec = {"dwi": NiftiGz, "bvec": File, "bval": File, "json": File, "direction": str}
 output_spec = {"dwi_convert_denoise_unring": File}
+# Create a workflow 
+wf = Workflow(name='connectomebids_carriage1_wf', input_spec=input_spec, cache_dir=output_path) 
 
+# organise bvec and bval into tuple
 @pydra.mark.task
 def merge_grads(bvec: File, bval: File) -> ty.List[File]:
     return [bvec, bval]
-
-# Create a workflow and add the mrconvert task
-wf = Workflow(name='connectomebids_carriage1_wf', input_spec=input_spec, cache_dir=output_path) 
 
 wf.add(
     merge_grads(
@@ -29,7 +29,7 @@ wf.add(
     )
 )
 
-# Convert to mif 
+# Convert to mif task
 wf.add(
     mrconvert(
         input=wf.lzin.dwi,
@@ -40,7 +40,7 @@ wf.add(
     )
 )
 
-# Apply dwidenoise
+# dwidenoise task
 wf.add(
     dwidenoise(
         name="denoise_node",
@@ -51,22 +51,27 @@ wf.add(
     )
 )
 
+# gibbs correction task
 wf.add(
     mrdegibbs(
         name="degibbs_node",
         in_=wf.denoise_node.lzout.out, 
-        out="denoised_degibbs.mif", 
+        out="dwi_denoise_degibbs.mif", 
         nshifts=50,
     )
 )
 
 wf.set_output(("carriage1_output", wf.degibbs_node.lzout.out))
 
-# Execute the workflow
-result = wf(
-    dwi="/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_dir-1_dwi.nii.gz",
-    bvec="/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_dir-1_dwi.bvec",
-    bval="/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_dir-1_dwi.bval",
-    json="/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_dir-1_dwi.json",
-    plugin="serial",
-)
+# ########################
+# # Execute the workflow #
+# ########################
+
+for direction in ["dir-1", "dir-2"]:
+    result = wf(
+        dwi=f"/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_{direction}_dwi.nii.gz",
+        bvec=f"/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_{direction}_dwi.bvec",
+        bval=f"/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_{direction}_dwi.bval",
+        json=f"/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/dwi/sub-01_{direction}_dwi.json",
+        plugin="serial",
+    )
