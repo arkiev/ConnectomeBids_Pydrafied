a964d1a8097ec2cc4dde194ab00474e51c3d0170
adsouza <arkievdsouza@sydney.edu.au>
2024-02-21 14:37:15 +1100

    added in conversions and epi_reg

--- ConnectomeBidsPydrafied_carriage3_v1.py
+++ ConnectomeBidsPydrafied_carriage3_v1.py
@@ -2,9 +2,11 @@ from ast import alias
 import os
 import typing as ty
 import pydra  
-from pydra import Workflow
+from pydra import Workflow, mark, ShellCommandTask
 from pydra.engine.specs import File
-from pydra.tasks.mrtrix3.v3_0 import mrconvert, mrgrid, dwi2mask_legacy, mrmath, mrthreshold, mrinfo, dwibiasnormmask
+from pydra.tasks.mrtrix3.v3_0 import mrconvert, mrgrid, mrconvert, dwiextract, mrcalc, mrmath, dwibiasnormmask, mrthreshold
+from pydra.tasks.fsl import epi_reg
+from pydra.engine.specs import SpecInfo, BaseSpec, ShellSpec, ShellOutSpec
 from fileformats.medimage import NiftiGzXBvec, NiftiGz
 from fileformats.medimage_mrtrix3 import ImageFormat
 from pathlib import Path
@@ -29,12 +31,11 @@ def run_mri_synthstrip():
     return result.stdout.decode()
 
 # Define the input_spec for the workflow
-input_spec = {"dwi_preproc_mif": File}
-output_spec = {"dwi_tmp": File}
+input_spec = {"dwi_preproc_mif": File, "FS_dir":str}
+output_spec = {"dwi_processed_registered": File}
 
 # Create a workflow 
 wf = Workflow(name='connectomebids_carriage3_wf', input_spec=input_spec, cache_dir=output_path, output_spec=output_spec) 
-# wf.add(run_mri_synthstrip())
 
 # Step 4: Generate an image containing all voxels where the DWI contains valid data
 # this step can be removed since the output is used in the iterative masking process (which is no long necessary)
@@ -85,7 +86,7 @@ wf.add(
     )
 )
 
-# Step 7: Crop images to reduce storage space (but leave some padding on the sides)
+# Step 7: Crop images to reduce storage space (but leave some padding on the sides) - pointing to wrong folder, needs fix (nonurgent)
 #grid DWI
 wf.add(
     mrgrid(
@@ -112,16 +113,179 @@ wf.add(
 
 # REPLACE Step8-10 with epi_reg (and transform DWI to T1 space)
 
-# Step 8: Generate target images for T1->DWI registration
-# Step 9: Perform DWI->T1 registration
-# Step 10: Perform DWI->T1 transformation
+########################
+# REGISTRATION CONTENT #
+########################
 
+# Step 8: Generate target images for T1->DWI registration
+
+@mark.task
+@mark.annotate({
+    "FS_dir": str,
+    "return": {
+        "t1_FSpath": str,
+        "t1brain_FSpath": str,
+        "wmseg_FSpath": str,
+        "normimg_FSpath": str,
+        }
+    })
+
+def join_task(FS_dir: str, output_path: Path):
+    t1_FSpath=os.path.join(FS_dir,"mri","T1.mgz")
+    t1brain_FSpath=os.path.join(FS_dir,"mri","brainmask.mgz")
+    wmseg_FSpath=os.path.join(FS_dir,"mri","wm.seg.mgz")
+    normimg_FSpath=os.path.join(FS_dir,"mri","T1.mgz")
+
+    return t1_FSpath, t1brain_FSpath, wmseg_FSpath, normimg_FSpath
+
+wf.add(join_task(FS_dir=wf.lzin.FS_dir, name="join_task"))
+
+# need to convert .mgz to nifti for registration
+wf.add(
+    mrconvert(
+        input=wf.join_task.lzout.t1_FSpath,
+        output="t1.nii.gz",
+        name="nifti_t1",
+    )
+)
+
+wf.add(
+    mrconvert(
+        input=wf.join_task.lzout.t1brain_FSpath,
+        output="t1brain.nii.gz",
+        name="nifti_t1brain",
+    )
+)
+
+wf.add(
+    mrconvert(
+        input=wf.join_task.lzout.wmseg_FSpath,
+        output="wmseg.nii.gz",
+        name="nifti_wmseg",
+    )
+)
+
+wf.add(
+    mrconvert(
+        input=wf.join_task.lzout.normimg_FSpath,
+        output="normimg.nii.gz",
+        name="nifti_normimg",
+    )
+)
+
+#######################
+# create meanb0 image #
+#######################
+
+wf.add(
+    dwiextract(
+        input=wf.dwibiasnormmask_task.output_dwi,
+        output="bzero.mif",
+        bzero=True,
+        name="extract_bzeroes_task",
+    )
+)
+
+
+# mrcalc spec info
+mrcalc_max_input_spec = SpecInfo(
+    name="Input",
+    fields=[
+    ( "image1", File,
+      { "help_string": "path to input image 1",
+        "argstr": "{image1}",
+        "mandatory": True,
+        "position": -4 } ),
+    ( "number", str,
+      { "help_string": "minimum value",
+        "argstr": "{number}",
+        "mandatory": True,
+        "position": -3 } ),
+    ( "operand", str,
+      { "help_string": "operand to execute",
+        "mandatory": True,
+        "position": -2 ,
+        "argstr": "-{operand}" }),
+    ( "output_image", str,
+      { "help_string": "path to output image",
+        "output_file_template": "output_image.nii.gz",
+        "argstr": "",
+        "mandatory": True,
+        "position": -1 } ),
+    ( "datatype", str,
+      { "help_string": "datatype option",
+        "argstr": "-datatype {datatype}" ,
+        "position": -5 } ),
+    ],
+    bases=(ShellSpec,) 
+)
+
+mrcalc_output_spec=SpecInfo(
+    name="Output",
+    fields=[
+    ( "output_image", str,
+      { "help_string": "path to output image",
+        "mandatory": True,
+        "position": -1 } ),
+    ],
+    bases=(ShellOutSpec,) 
+)
+
+# remove negative values from bzero volumes
+wf.add(
+    ShellCommandTask(
+        name="mrcalc_max",
+        executable="mrcalc",
+        input_spec=mrcalc_max_input_spec, 
+        output_spec=mrcalc_output_spec, 
+        cache_dir=output_path,
+        image1=wf.extract_bzeroes_task.lzout.output,
+        number="0.0",
+        operand='max',
+        output_image='bzero_positive.mif'
+    )
+)
+
+# create meanb0 image
+
+wf.add(
+    mrmath(
+        input=wf.mrcalc_max.lzout.output_image,
+        output="dwi_meanbzero.nii.gz",
+        name="meanb0_task",
+        operation="mean",
+        axis=3,
+    )
+)
+
+# Step 9: Perform DWI->T1 registration
+
+wf.add(
+    epi_reg(
+        epi=wf.meanb0_task.lzout.output,
+        t1_head=wf.nifti_t1.lzout.output,
+        t1_brain=wf.nifti_t1brain.lzout.output,
+        wmseg=wf.nifti_wmseg.lzout.output,
+        out_base="dwi2T1_warp.nii.gz",
+        name="epi_reg_task"
+        
+    )
+)
+
+# transformconvert task 
+wf.add(
+    transformconvert(
+
+    )
+)
+# Step 10: Perform DWI->T1 transformation
+# mrconvert
 
 # wf.set_output(("carriage3_output", wf.dwibiasnormmask_task.lzout.output_dwi))
 # wf.set_output(("carriage3_output", wf.crop_task_mask.lzout.output))
 # wf.set_output(("carriage3_output", wf.crop_task_dwi.lzout.output))
 
-wf.set_output(("carriage3_output", wf.dwibiasnormmask_task.lzout.output_dwi))
+wf.set_output(("carriage3_output", wf.epi_reg_task.lzout.out_base))
 
 # ########################
 # # Execute the workflow #
@@ -129,6 +293,6 @@ wf.set_output(("carriage3_output", wf.dwibiasnormmask_task.lzout.output_dwi))
 
 # wf = connectomebids_carriage3_wf(direction=direction)
 result = wf(dwi_preproc_mif="/Users/arkievdsouza/Desktop/ConnectomeBids/output/MRtrix3_connectome-preproc/sub-01/dwi/sub-01_desc-preproc_dwi.mif.gz",
-    # T1_image="/Users/arkievdsouza/Desktop/ConnectomeBids/data/sub-01/anat/sub-01_T1w.nii.gz",
+    FS_dir="/Users/arkievdsouza/Desktop/ConnectomeBids/output/MRtrix3_connectome-participant_hcpmmp1/sub-01/scratch/freesurfer/",
     plugin="serial",
 )
